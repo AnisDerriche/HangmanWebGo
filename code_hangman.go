a0954ddf7cb1c47fe943b653fd9735525a52c3e4
Coatleven <rafal.coat@gmail.com>
2024-12-10 09:16:15 +0100

    en local

---
+++ code_hangman.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	t.Execute(w, nil)
+}
+
+func Jeux(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/jeux.html"))
+	t.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/jeux", Jeux)
+	fmt.Println("http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
